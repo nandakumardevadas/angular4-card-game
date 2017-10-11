74df2db1ea6eeb7035bdbd3f81bffc4a76d0b78a
Nandakumar D <nandakumard@ihorsetechnologies.com>
2017-10-11 18:17:34 +0530

    Baraja added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import mojs from 'mojs';
+declare var $ :any;
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,38 @@ import mojs from 'mojs';
 })
 export class HomeComponent implements OnInit {
 
+  baraja: any;
   constructor() { }
 
   ngOnInit() {
-    
+    var $el = $( '#baraja-el' );
+    this.baraja = $el.baraja();
+  }
+
+  fanRight() {
+    this.baraja.fan( {
+      speed : 500,
+      easing : 'ease-out',
+      range : 90,
+      direction : 'right',
+      origin : { x : 25, y : 100 },
+      center : true
+    });
+  }
+
+  centeredRound() {
+    this.baraja.fan( {
+      speed : 500,
+      easing : 'ease-out',
+      range : 360,
+      direction : 'right',
+      origin : { x : 50, y : 90 },
+      center : false
+    } );
+  }
+
+  close() {
+    this.baraja.close();
   }
 
 }
